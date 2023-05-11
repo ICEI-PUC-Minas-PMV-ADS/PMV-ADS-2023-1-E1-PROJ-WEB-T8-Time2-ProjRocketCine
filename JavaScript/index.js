a1544d119191eba9510c4916ea3ba17f86bd4137
Wesley Correa Gomes <wesleycorrea1298@gmail.com>
2023-05-10 23:33:48 -0300

    Atualização e emplementação da api de filmes

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,20 +1,20 @@
 
 // Remover o modal ao clicar em algum botão
-function removerModal(modalID){
+function removerModal(modalID) {
     const modal = document.getElementById(modalID)
 
-    modal.addEventListener('click', function(remover){
-        if(remover.target.id == modalID){
+    modal.addEventListener('click', function (remover) {
+        if (remover.target.id == modalID) {
             modal.classList.remove('mostrar')
         }
     })
 }
 
 // Abre o modal e remove ao clicar fora da área de login ou cadastro
-function iniciarModal(modalID){
+function iniciarModal(modalID) {
     const modal = document.getElementById(modalID)
     modal.classList.add('mostrar')
-    
+
     removerModal(modalID)
 }
 
@@ -22,38 +22,38 @@ function iniciarModal(modalID){
 const cadastrar = document.getElementById('cadastrar')
 const entrar = document.getElementById('entrar')
 
-entrar.addEventListener('click', function(){
+entrar.addEventListener('click', function () {
     iniciarModal('modal_login')
 })
 
-cadastrar.addEventListener('click', function(){
-     iniciarModal('modal_cadastro')
+cadastrar.addEventListener('click', function () {
+    iniciarModal('modal_cadastro')
 })
 
 // Coletando e validando os dados de cadastro
-function coletandoValidandoDadosCadastro () {
-    
+function coletandoValidandoDadosCadastro() {
+
     var nome = document.getElementById('nome')
     var email = document.getElementById('email')
     var senha1 = document.getElementById('senha1')
     var senha2 = document.getElementById('senha2')
     const criar = document.getElementById('criar')
 
-    criar.addEventListener('click', function(){
+    criar.addEventListener('click', function () {
 
-        if(senha1.value != senha2.value){  
+        if (senha1.value != senha2.value) {
             alert('As senhas não são iguais')
             senha1.classList.add('erro')
-            senha2.classList.add('erro')        
-        }
-        
-        else if(senha1.value.length < 4 && senha2.value.length < 4){
-            alert('A senha deve conter no minimo 4 caracteres')
-            senha1.classList.add('erro')
-            senha2.classList.add('erro')   
+            senha2.classList.add('erro')
         }
 
-        else{
+        else if (senha1.value.length < 4 && senha2.value.length < 4) {
+            alert('A senha deve conter no minimo 4 caracteres')
+            senha1.classList.add('erro')
+            senha2.classList.add('erro')
+        }
+
+        else {
             localStorage.setItem('nome', nome.value)
             localStorage.setItem('email', email.value)
             localStorage.setItem('senha1', senha1.value)
@@ -61,18 +61,15 @@ function coletandoValidandoDadosCadastro () {
 
             document.getElementById('modal_cadastro').classList.remove('mostrar')
             alert('Conta criada com sucesso!')
-            setTimeout(() => {  iniciarModal('modal_login') }, 300)
+            setTimeout(() => { iniciarModal('modal_login') }, 300)
 
-            }
+        }
     })
 }
 
 
-
-
-
 // Coletando e validando os dados de login
-function coletandoValidandoDadosLogin () {
+function coletandoValidandoDadosLogin() {
 
     const perfilID = document.getElementById('perfilID')
     const entrar_cadastrarID = document.getElementById('entrar_cadastrarID')
@@ -84,27 +81,26 @@ function coletandoValidandoDadosLogin () {
     const senha = document.getElementById('senha_login')
     const logar = document.getElementById('logar')
 
-    
 
-    logar.addEventListener('click', function(){
+    logar.addEventListener('click', function () {
 
-        if(email.value == localStorage.getItem('email') && senha.value == localStorage.getItem('senha1')){
+        if (email.value == localStorage.getItem('email') && senha.value == localStorage.getItem('senha1')) {
 
             alert('Login realizado com sucesso!')
             document.getElementById('modal_login').classList.remove('mostrar')
             entrar_cadastrarID.classList.add('logar')
             perfilID.classList.remove('logar')
-            
+
             //Chamando animação ao clicar no perfil
-            perfifoto.addEventListener('click', function(){
+            perfifoto.addEventListener('click', function () {
                 perfifoto.style.animation = ''
-                setTimeout(() => perfifoto.style.animation = 'perfil .1s' , .5)
+                setTimeout(() => perfifoto.style.animation = 'perfil .1s', .5)
             })
 
             quem_entrou.appendChild(texto)
-            
+
         }
-        else if(email.value != localStorage.getItem('email') || senha.value != localStorage.getItem('senha')){
+        else if (email.value != localStorage.getItem('email') || senha.value != localStorage.getItem('senha')) {
             alert('E-mail ou senha incorreto')
             email.classList.add('erro')
             senha.classList.add('erro')
@@ -115,3 +111,4 @@ function coletandoValidandoDadosLogin () {
 
 coletandoValidandoDadosCadastro()
 coletandoValidandoDadosLogin()
+
